ee8572b64de4f98c7cb68f18c65c84b00e9f179c
leon332157 <leon332157@gmail.com>
2023-12-01 03:21:12 -0500

    working, not the best however

--- step-tracker/step-tracker.ino
+++ step-tracker/step-tracker.ino
@@ -15,17 +15,13 @@
 #define PRINT_INTERVAL 200
 #define RST_BUTTON 9
 
-#define MAG_ARRAY_LEN 100
+#define MAG_ARRAY_LEN 200
 
 typedef float64_t float64;
 
 char printBuffer[PRINT_BUFFER_LEN] = "";
 
 Adafruit_MPU6050 mpu;
-Smoothed<int16_t> accelX;
-Smoothed<int16_t> accelY;
-Smoothed<int16_t> accelZ;
-
 typedef struct {
   float arr[MAG_ARRAY_LEN] = {0};
   unsigned int size;
@@ -33,11 +29,9 @@ typedef struct {
 
 mag_array_t magArray;
 
-float64 runningAvg = float64_NUMBER_PLUS_ZERO;
-float64 runningStddev = float64_NUMBER_PLUS_ZERO;
-
 unsigned int peaks = 0;
-
+unsigned long prevPrint = 0;
+unsigned long lastPeak = 0;
 // calculate the mean of the current values in the mag array
 float mean() {
   // Serial.println("mean");
@@ -46,11 +40,11 @@ float mean() {
     sum = sum + magArray.arr[i];
   }
   float avg = sum / magArray.size;
-  Serial.print("avg:");
+  Serial.print(">avg:");
   Serial.println(avg);
-  Serial.print("sum:");
+  Serial.print(">sum:");
   Serial.println(sum);
-  Serial.print("size:");
+  Serial.print(">size:");
   Serial.println(magArray.size);
 
   return avg;
@@ -69,7 +63,7 @@ float stddev(float mu) {
   }
   sigma = sigma / magArray.size;
   sigma = sqrt(sigma);
-  Serial.print("sigma:");
+  Serial.print(">sigma:");
   Serial.println(sigma);
   return sigma;
 }
@@ -84,16 +78,23 @@ float stddev_mu() {
 float zscore(float v) {
   // Serial.println("zscore");
   float mu = mean();
-  float sigma = stddev(mu);
+   float sigma = stddev(mu);
   float zscore = (v - mu) / sigma;
-  Serial.print("zscore:");
+  Serial.print(">zscore:");
   Serial.println(zscore);
   return zscore;
 }
 
-bool isPeak(float v) {
+unsigned char isPeak(float v) {
+  // Serial.println("isPeak");
+  if (millis() - lastPeak < 150) {
+    return 0;
+  }
   float z = zscore(v);
-  return z > 2 || z < -2;
+  if (z > 4) {
+    lastPeak = millis();
+    return 1;
+  }
 }
 
 void addMag(float mag) {
@@ -107,7 +108,7 @@ void addMag(float mag) {
     }
     magArray.arr[MAG_ARRAY_LEN - 1] = mag;
   }
-  if (isPeak(mag)) {
+  if (1 == isPeak(mag)) {
     peaks++;
   }
 }
@@ -115,37 +116,15 @@ void addMag(float mag) {
 void printOLED() {
   ssd1306_setCursor(0, 0);
   char buf[16];
-  sprintf(buf, "Peaks: %u", peaks);
+  sprintf(buf, ">Steps: %u", peaks);
   ssd1306_print(buf);
   Serial.println(buf);
 }
 
 void print() {
   char fbuf[32];
-
-  sprintf(printBuffer, "AccelX:%i, AccelY:%i, AccelZ:%i", accelX.get(),
-          accelY.get(), accelZ.get());
   // Serial.println(printBuffer);
   printOLED();
-}
-
-void printFast() {
-  Serial.print("AccelX:");
-  Serial.print(accelX.get());
-  Serial.print(",");
-  Serial.print("AccelY:");
-  Serial.print(accelY.get());
-  Serial.print(",");
-  Serial.print("AccelZ:");
-  Serial.print(accelZ.get());
-  Serial.println();
-}
-
-void printTelePlot() {
-  char buf1[128];
-  sprintf(buf1, ">AccelX:%li\n>AccelY:%li\n>AccelZ:%li\n", accelX.get(),
-          accelY.get(), accelZ.get());
-  Serial.print(buf1);
 }
 
 void setup(void) {
@@ -169,18 +148,12 @@ void setup(void) {
   mpu.setTemperatureStandby(true);
   mpu.enableSleep(false);
 
-  accelX.begin(SMOOTHED_AVERAGE, 30);
-  accelY.begin(SMOOTHED_AVERAGE, 30);
-  accelZ.begin(SMOOTHED_AVERAGE, 30);
-
   ssd1306_128x32_i2c_init();
   ssd1306_clearScreen();
   ssd1306_setFixedFont(ssd1306xled_font6x8);
   pinMode(RST_BUTTON, INPUT_PULLUP);
   memset(&magArray, 0, sizeof(magArray));
 }
-
-unsigned long prevPrint = 0;
 
 void loop() {
   /* Get new sensor events with the readings */
@@ -190,9 +163,6 @@ void loop() {
   accel_t accel_raw;
 
   mpu.getAccelEventRaw(&accel_raw);
-  accelX.add(accel_raw.x);
-  accelY.add(accel_raw.y);
-  accelZ.add(accel_raw.z);
   int16_t x = accel_raw.x;
   int16_t y = accel_raw.y;
   int16_t z = accel_raw.z;
@@ -200,9 +170,12 @@ void loop() {
       fp64_sqrt(fp64_add(fp64_square(fp64_int16_to_float64(x)),
                          fp64_add(fp64_square(fp64_int16_to_float64(y)),
                                   fp64_square(fp64_int16_to_float64(z)))));
-  Serial.print("mag:");
-  Serial.println(fp64_ds(mag));
-  addMag(fp64_ds(mag));
+  // smoothedMag.add(fp64_ds(mag));
+  // float smmag = smoothedMag.get();
+  float smmag = fp64_ds(mag);
+  Serial.print(">mag:");
+  Serial.println(smmag);
+  addMag(smmag);
   if (digitalRead(RST_BUTTON) == LOW) {
     peaks = 0;
     ssd1306_clearScreen();
